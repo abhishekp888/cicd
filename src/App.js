580b073f87961c3386b73b2cc95d9670cd726fe3
abhishekp888 <114831387+abhishekp888@users.noreply.github.com>
2023-10-20 20:29:02 +0530

    prasad

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prasad ki gand thick hai
         </p>
         <a
           className="App-link"
